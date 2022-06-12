a32f83426c42d3e64b3551833c2c806a8ec7c734
rykci <yuen.ricky@hotmail.com>
2022-06-12 17:12:06 -0400

    hello api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).send(req.ip)
+  res
+    .status(200)
+    .send(req.headers['x-forwarded-for'] || req.socket.remoteAddress)
 }
